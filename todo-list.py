16c28fe341dfcfc4c70f5d482bee1b08d0622afd
aashlemon <74264156+aashlemon@users.noreply.github.com>
2021-10-01 19:13:56 +0000

    added more code and stuff to the main file.

--- todo-list.py
+++ todo-list.py
@@ -1,18 +1,31 @@
+#init variables
 running = True
 file = None
 
+# the main code
 while running:
-  userinp = input()
-  userinparg = userinp.split()
   try:
+    # getting the user input
+    userinp = input()
+    # spliting into args
+    userinparg = userinp.split()
+
     if userinparg[0] == "open":
-      file = open(userinparg[1], "r+")
+      file = open(userinparg[1], "a+")
+    if userinparg[0] == "add-task":
+      pass
     elif userinparg[0] == "quit":
       running = False
     else:
       print("command not found")
-  except IndexError:
+  except IndexError: # error handling stuff
     print("please enter a command")
+  except EOFError:
+    print("\nplease enter a command")
+  except KeyboardInterrupt:
+    print("\nplease enter a command")
+  except OSError:
+    print("open a list file first to use that command")
 
 try:
   file.close()
