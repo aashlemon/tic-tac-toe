2d82211be1e6165d83522ae85756bfb0da9014c9
aashlemon <74264156+aashlemon@users.noreply.github.com>
2021-10-05 21:47:46 +0000

    even more code in main

--- todo-list.py
+++ todo-list.py
@@ -1,8 +1,8 @@
 from os import system
-from random import randrange
 
 running = True
 file = None
+filename = None
 
 def format_task(task): # will format the task item in file in a neat way.
     result = "task: " + task.split("|")[0] + "id:  " + task.split("|")[1]
@@ -13,18 +13,13 @@ while running:
     try:
       usrinp = input()
       usrinpargs = usrinp.split()
-      filesplit = None # initizialing the filetxt stuff to be global
-
-      if file != None:
-        filesplit = file.read().split(";")
     
       if usrinpargs[0] == "open" or usrinpargs[0] == "create":
         file = open(usrinpargs[1], "a+")
       elif usrinpargs[0] == "print":
-        for i in filesplit:
-          print(i)
+        print(file.read())
       elif usrinpargs[0] == "add-task":
-        file.write(usrinpargs[1] + ";")
+        file.write("".join(usrinpargs[1:]) + ";")
       elif usrinpargs[0] == "clear": # gets really annoying when so much stuff on the screen
         try:
           system("clear") # unix
@@ -39,3 +34,7 @@ while running:
         print("please enter a command or put in more args")
     except (OSError, TypeError, AttributeError): # if user hasn't opened a todo list file yet
         print("must first open a file to use that command")
+
+    file.close()
+
+file.close()
