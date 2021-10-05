aad7618930c9ffa5e6834989f398e19c8c25d755
aashlemon <74264156+aashlemon@users.noreply.github.com>
2021-10-05 16:23:18 +0000

    even more code to the main file.

--- todo-list.py
+++ todo-list.py
@@ -1,4 +1,5 @@
 from os import system
+from random import randrange
 
 running = True
 file = None
@@ -17,20 +18,24 @@ while running:
       if file != None:
         filesplit = file.read().split(";")
     
-        if usrinpargs[0] == "open" or usrinpargs[0] == "create":
-           file = open(usrinpargs[1], "r+")
-        elif usrinpargs[0] == "print":
-           for i in filesplit:
-             print(format(i))
-        elif usrinpargs[0] == "clear": # gets really annoying when so much stuff on the screen
-          try:
-            system("clear") # unix
-          except OSError:
-            system("cls") # windows
-            
+      if usrinpargs[0] == "open" or usrinpargs[0] == "create":
+        file = open(usrinpargs[1], "a+")
+      elif usrinpargs[0] == "print":
+        for i in filesplit:
+          print(i)
+      elif usrinpargs[0] == "add-task":
+        file.write(usrinpargs[1] + ";")
+      elif usrinpargs[0] == "clear": # gets really annoying when so much stuff on the screen
+        try:
+          system("clear") # unix
+        except OSError:
+          system("cls") # windows
+      else:
+        print("command not found")
+
     except EOFError: # error handling
         print("\nplease enter a command")
     except IndexError: # more error handling
-        print("please enter a command")
-    except OSError: # if user hasn't opened a todo list file yet
-        print("must first open a file to use that command")
+        print("please enter a command or put in more args")
+    #except (OSError, TypeError, AttributeError): # if user hasn't opened a todo list file yet
+    #    print("must first open a file to use that command")
