e9c3b588b9007f7530a9988080764b74ca12844e
aashlemon <74264156+aashlemon@users.noreply.github.com>
2021-10-06 17:48:12 +0000

    added code

--- todo-list.py
+++ todo-list.py
@@ -4,8 +4,16 @@ running = True
 tasks = {
 
 }
+notify = 0
 
 while running:
+    if notify == 3:
+            if len(tasks) != 0:
+                print("Number of task left: " + len(tasks))
+
+    if len(tasks) == 0:
+        print("Done for the day!")
+
     try:
         usrinp = input("Enter in a command: ")
         inpargs = usrinp.split()
@@ -13,7 +21,10 @@ while running:
         if inpargs[0] == "quit":
             running = False
         elif inpargs[0] == "clear":
-            system("clear")
+            try:
+                system("clear")
+            except OSError:
+                system("cls")
         else:
             print("command not found")
     except IndexError:
